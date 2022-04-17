a1f55f498747136814dd13022672a4d52118b8d3
Swumplurd <53883345+Swumplurd@users.noreply.github.com>
2022-04-16 19:53:35 -0500

    primer commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link href="{{asset('./styles/app.css')}}" rel="stylesheet">
+    <title>Agenda de contactos</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <nav class="nav d-flex justify-content-between">
+                    <div>
+                        <a class="nav-link px-0" aria-current="page" href="/">agenda de contactos 📱</a>
+                    </div>
+                    <div class='d-flex'>
+                        <a class="nav-link" href="/">añadir categoria</a>
+                        <a class="nav-link" href="/">añadir contacto</a>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-6">
+                <h1 class="display-3 fw-bold">Bienvenido a tu agenda personal</h1>
+            </div>
+            <div class="col-6">
+                <img class="img-fluid" src="{{asset('./img/undraw_personal_email_re_4lx7.svg')}}" alt="contact-svg" srcset="">
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+</body>
+</html>
